612efe5cbb1773e8fcd63b17ee4103469cd88a50
Man bls bls <125179236+AvidTroll@users.noreply.github.com>
2023-04-12 20:14:49 +0000

    Proxy

--- public/proxy.js
+++ public/proxy.js
@@ -1,24 +1,8 @@
-function searchAndLoad() {
-  var input = document.getElementById('searchbar').value;
-  if (input) {
-    // Run the go() function with the input value
-    go();
-
-    // Create a new iframe element and set its attributes
-    var iframe = document.createElement('iframe');
-    iframe.setAttribute('src', '/main/' + input);
-    iframe.setAttribute('frameborder', '0');
-    iframe.setAttribute('allowfullscreen', '');
-    iframe.style.position = 'fixed';
-    iframe.style.top = '0';
-    iframe.style.left = '0';
-    iframe.style.width = '100%';
-    iframe.style.height = '100%';
-
-    // Replace the current page with the iframe
-    document.body.innerHTML = '';
-    document.body.appendChild(iframe);
-  } else {
-    alert('Please provide a link. It does not require https://.');
-  }
-}
+                function go() {
+                    var input = document.getElementById('searchbar');
+                    if (input.value) {
+                        window.location.href = '/main/' + input.value;
+                    } else {
+                        alert('Please provide a link. It does not require https://.');
+                    }
+                }
